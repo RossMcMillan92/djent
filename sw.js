4d220a64f064addf1938d0696c34fe466a13c7f0
Ross McMillan <ross.mcmillan@stv.tv>
2016-07-26 10:02:14 +0100

    fix sw

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
 /* eslint no-undef: 0 */
-console.log('IMPORTSCRIPTS', importScripts)
-importScripts('/node_modules/sw-toolbox/sw-toolbox.js')
-    .catch(e => console.log(e));
+importScripts('/node_modules/sw-toolbox/sw-toolbox.js');
 
-// toolbox.options.debug = true;
+toolbox.options.debug = true;
 
-toolbox.precache(['/*', 'https://raw.githubusercontent.com/RossMcMillan92/djent/master/assets/audio/*']);
+toolbox.precache(['/index.html', 'https://raw.githubusercontent.com/RossMcMillan92/djent/master/assets/audio/*']);
 
-toolbox.router.get('/*', toolbox.networkFirst);
+toolbox.router.get('/index.html', toolbox.networkFirst);
 toolbox.router.get('https://raw.githubusercontent.com/RossMcMillan92/djent/master/assets/audio/*', toolbox.fastest);

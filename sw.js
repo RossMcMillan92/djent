784e945e90024423e97b36cc52ba5ecb0607a41d
Ross McMillan <rossmcmillan92@gmail.com>
2016-08-29 23:58:15 +0100

    update sw

--- sw.js
+++ sw.js
@@ -1,27 +1,5 @@
 /* eslint no-undef: 0 */
 /* eslint strict: 0 */
-// importScripts('/node_modules/sw-toolbox/sw-toolbox.js');
-// toolbox.options.debug = false;
-//
-// toolbox.precache([
-//     '/index.html',
-//     '/dist/*',
-//     // 'https://raw.githubusercontent.com/RossMcMillan92/djent/master/assets/audio/*',
-// ]);
-//
-// toolbox.router.get('/index.html', toolbox.networkFirst);
-// toolbox.router.get('/assets/(.*)', toolbox.fastest);
-// toolbox.router.get('/dist/(.*)', toolbox.networkFirst);
-// toolbox.router.get('https://raw.githubusercontent.com/RossMcMillan92/djent/master/assets/(.*)', toolbox.fastest);
-
-// toolbox.router.get('/css(.*)', self.toolbox.fastest, {
-//     origin: /fonts\.googleapis\.com/,
-//     cache: {
-//         name: 'dynamic-vendor-cache-v1',
-//         maxEntries: 5
-//     }
-// });
-
 
 (global => {
     'use strict';

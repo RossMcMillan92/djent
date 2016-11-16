8a4299a9cae14c176b8702eb19eb439a822e448d
Ross McMillan <ross.mcmillan@stv.tv>
2016-11-16 17:11:32 +0000

    refactor export controller

--- src/scripts/app/components/ExportController.js
+++ src/scripts/app/components/ExportController.js
@@ -7,10 +7,20 @@ import { buildMidiDataURIFromInstruments } from '../utils/midi';
 const ExportModal = ({ actions }) => (
     <div className="u-flex-row u-flex-wrap">
         <div className="u-mr05">
-            <button className="button-primary button-primary--small button-primary--positive" onClick={ () => { actions.saveMIDI(); actions.disableModal(); } } >Save as MIDI</button>
+            <button
+                className="button-primary button-primary--small button-primary--positive"
+                onClick={ () => { actions.saveMIDI(); actions.disableModal(); } }
+            >
+                Save as MIDI
+            </button>
         </div>
         <div className="">
-            <button className="button-primary button-primary--small button-primary--positive" onClick={ () => { actions.saveWAV(); actions.disableModal(); } } >Save as WAV</button>
+            <button
+                className="button-primary button-primary--small button-primary--positive"
+                onClick={ () => { actions.saveWAV(); actions.disableModal(); } }
+            >
+                Save as WAV
+            </button>
         </div>
     </div>
 );
@@ -20,20 +30,25 @@ const buildAndSaveMidi = compose(
     buildMidiDataURIFromInstruments,
 );
 
+const launchExportModal = (instruments, bpm, buffer, actions) => {
+    const content = (
+        <ExportModal actions={{
+            disableModal: actions.disableModal,
+            saveMIDI: () => buildAndSaveMidi(instruments, bpm),
+            saveWAV: () => saveAsWAVFile(buffer),
+        }} />
+    );
+    actions.enableModal({ content, isCloseable: true, title: 'Export' });
+}
+
 const ExportController = props => {
     const { instruments, bpm, buffer, actions } = props;
-    const launchExportModal = () => {
-        const content = (
-            <ExportModal actions={{
-                disableModal: actions.disableModal,
-                saveMIDI: () => buildAndSaveMidi(instruments, bpm),
-                saveWAV: () => saveAsWAVFile(buffer),
-            }} />
-        );
-        actions.enableModal({ content, isCloseable: true, title: 'Export' });
-    }
     return (
-        <button className="button-primary button-primary--alpha-dark button-primary--tiny" onClick={launchExportModal} disabled={!buffer}>
+        <button
+            className="button-primary button-primary--alpha-dark button-primary--tiny"
+            onClick={() => launchExportModal(instruments, bpm, buffer, actions)}
+            disabled={!buffer}
+        >
             Save
         </button>
     );

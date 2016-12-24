3dcb3c64c9ee8330f716c59def0fa21cb3d19b0f
Ross McMillan <rossmcmillan92@gmail.com>
2016-12-24 12:03:48 +0000

    made meshuggah default

--- src/generic/scripts/app/reducers/config.js
+++ src/generic/scripts/app/reducers/config.js
@@ -1,48 +1,48 @@
-import { extendObjectArrayByID } from 'utils/tools';
+import { extendObjectArrayByID } from 'utils/tools'
 
 const initialState = {
-    activePresetID       : 'contortionist',
+    activePresetID       : 'meshuggah',
     bpm                  : 50,
     continuousGeneration : false,
-};
+}
 
 export default function config(state = { ...initialState }, action) {
-    const { type, payload } = action;
+    const { type, payload } = action
 
     switch (type) {
         case 'UPDATE_BPM':
             return {
                 ...state,
                 bpm: payload.bpm
-            };
+            }
 
         case 'UPDATE_CONTINUOUS_GENERATION':
             return {
                 ...state,
                 continuousGeneration: payload.continuousGeneration
-            };
+            }
 
         case 'APPLY_PRESET':
-            const { preset } = payload;
-            const { bpm, allowedLengths, hitChance } = preset.settings.config;
-            const newState = { ...initialState };
+            const { preset } = payload
+            const { bpm, allowedLengths, hitChance } = preset.settings.config
+            const newState = { ...initialState }
 
-            if (bpm) newState.bpm = bpm;
-            if (hitChance) newState.hitChance = hitChance;
+            if (bpm) newState.bpm = bpm
+            if (hitChance) newState.hitChance = hitChance
             if (allowedLengths) {
-                newState.allowedLengths = extendObjectArrayByID(newState.allowedLengths, [ ...allowedLengths ]);
+                newState.allowedLengths = extendObjectArrayByID(newState.allowedLengths, [ ...allowedLengths ])
             }
 
             return {
                 ...newState,
                 activePresetID: preset.id
-            };
+            }
 
         default:
-            return state;
+            return state
   }
 }
 
 export {
     initialState
-};
+}
